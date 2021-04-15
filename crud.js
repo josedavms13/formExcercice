212c687124f30a8f5cc3db171f1cb639ec05b73a
josedavms13 <josedav0ms13@gmail.com>
2021-04-14 21:59:37 -0500

    exercices

---
+++ crud.js
@@ -0,0 +1,76 @@
+const users = [
+    {
+        name: 'Erik',
+        age: 29,
+        email: 'erik@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/erik' },
+            { name: 'twitter', url: 'twitter/erik' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Georg',
+        age: 33,
+        email: 'georg@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/georg' },
+            { name: 'twitter', url: 'twitter/georg' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Andrea',
+        age: 42,
+        email: 'andrea@hotmail.com',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+    {
+        name: 'Oscar',
+        age: 31,
+        email: 'oscar@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/oscar' },
+            { name: 'twitter', url: 'twiter/oscar' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Daniela',
+        age: 22,
+        email: 'daniela@uaq.mx',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+]
+
+
+
+function printUsers(users){
+    const container = document.getElementById('users-container');
+    container.innerHTML = '';
+    users.forEach((element)=>{
+        const toprintHtml = `
+        <tr>
+            <td scope="col">${element.name}</td>
+            <td scope="col">${element.email}</td>
+        </tr>
+        `;
+        container.innerHTML += toprintHtml;
+    })
+}
+
+printUsers(users);
+
+
+
+function showForm(){
+    const 
+}
